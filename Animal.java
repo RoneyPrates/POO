bf0835f14719fc510dec01cf574ff183fb73cbe6
Roney <107938728+RoneyPrates@users.noreply.github.com>
2023-03-08 19:39:48 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,52 @@
+package com.mycompany.animal;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author roney
+ * RA 00234988
+ */
+
+public class Animal {
+    public static void main(String[] args) {
+        Gaviao Juca = new Gaviao();
+        Juca.cor = "preto";
+        Juca.especie = "Gavião Preto";
+        Juca.idade = 18;
+        Juca.tamanho = 1.5f;
+        Juca.sexo = 'M';
+               
+        Gaviao Jose = new Gaviao();
+        Jose.cor = "preto e branco";
+        Jose.especie = "Falcão de Coleira";
+        Jose.idade = 7;
+        Jose.tamanho = 4f;
+        Jose.sexo = 'M';
+        
+        Gaviao Hana = new Gaviao();
+        Hana.cor = "branca";
+        Hana.especie = "Águia Pesqueira";
+        Hana.idade = 39;
+        Hana.tamanho = 2.1f;
+        Hana.sexo = 'F';
+        
+        Gaviao Fer = new Gaviao();
+        Fer.cor = "preto";
+        Fer.especie = "Búteo de cauda vermelha";
+        Fer.idade = 18;
+        Fer.tamanho = 1.5f;
+        Fer.sexo = 'M';
+        
+        Gaviao Sorriso = new Gaviao();
+        Sorriso.cor = "marrom";
+        Sorriso.especie = "Falcão Americano";
+        Sorriso.idade = 25;
+        Sorriso.tamanho = 0.7f;
+        Sorriso.sexo = 'M';
+    }
+    
+}
---
+++ Gaviao.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.animal;
+
+/**
+ *
+ * @author aluno
+ */
+public class Gaviao {
+
+        String cor;
+        float tamanho;
+        String especie;
+        int idade;
+        char sexo;
+    
+    }
+
